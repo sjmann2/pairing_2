2b4be826d7c5b504104ba2ad9022e088b08f3368
Sid Mann <sjmann2@outlook.com>
2022-06-08 14:45:06 -0500

    New friend emily

--- friends.rb
+++ friends.rb
@@ -1,6 +1,12 @@
 best_friend = "chelsea"
+friend_2 = "emily"
 if best_friend == "chelsea"
   puts "Hi chelsea"
 elsif best_friend != "chelsea"
   puts "Hey stranger"
+end
+if friend_2 == "emily"
+  puts "How are you doing emily?"
+elsif friend_2 != "emily"
+  puts "How did you get this login?"
 end
